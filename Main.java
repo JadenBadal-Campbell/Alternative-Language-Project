5db8851bcb9f3888b5dd3cf8d33d68c36459dea3
JadenBadal-Campbell <94206644+JadenBadal-Campbell@users.noreply.github.com>
2024-04-04 09:02:29 -0400

    Basic CSV Reading and Writing

--- Main.java
+++ Main.java
@@ -10,7 +10,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        String inputCsvFile = "cells.csv"; // Path to your input CSV file
+        String outputCsvFile = "cells_sanitized.csv"; // Path to your output CSV file
+
+        try {
+            FileReader fileReader = new FileReader(inputCsvFile);
+            CSVParser parser = new CSVParser(fileReader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+            Writer writer = Files.newBufferedWriter(Paths.get(outputCsvFile));
+
+            // Writing header to output CSV
+            writer.write(String.join(",", parser.getHeaderNames()) + "\n");
+
+           
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
+
 
  
